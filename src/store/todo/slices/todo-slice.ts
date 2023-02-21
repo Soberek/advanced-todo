fa88204580c7161b6e1baffc0c7fcf35a2ef5a95
Soberek <59960384+Soberek@users.noreply.github.com>
2023-02-21 18:38:20 +0100

    feat: todo list slice

---
+++ src/store/todo/slices/todo-slice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit/dist/createSlice';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface Todo {
+	id: string;
+	title: string;
+}
+
+interface TodoList {
+	todos: Todo[];
+}
+
+const initialState: TodoList = {
+	todos: [],
+};
+
+const todoSlice = createSlice({
+	name: 'todos',
+	initialState,
+	reducers: {
+		add: (state, action: PayloadAction<Todo>) => {
+			state.todos.push(action.payload);
+		},
+		remove: (state, action) => {},
+		update: (state, action) => {},
+		complete: (state, action) => {},
+	},
+});
+
+export const { add, remove, update, complete } = todoSlice.actions;
+export default todoSlice.reducer;
