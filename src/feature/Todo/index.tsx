77fd266558938dc0796910b1ef423fab1573996a
Soberek <59960384+Soberek@users.noreply.github.com>
2023-02-22 16:03:38 +0100

    style: removed gradient from bg

--- src/feature/Todo/index.tsx
+++ src/feature/Todo/index.tsx
@@ -12,13 +12,7 @@ const Main = () => {
 			<Box
 				minHeight='100vh'
 				sx={{
-					backgroundImage: `
-                    linear-gradient(to bottom, rgba(255,255,255, 0) 10%, ${theme.palette.primary.dark} 100%,  ${
-						theme.palette.primary.dark
-					} 100%),
-                    // linear-gradient(to left, rgba(255,0,0,0) 0%, ${alpha(theme.palette.primary.dark, 0.7)} 100%), 
-                    url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')
-                    `,
+					backgroundImage: `url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')`,
 					backgroundSize: 'cover',
 					backgroundPosition: 'center',
 					backgroundAttachment: 'fixed',
