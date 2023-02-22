e9858544cd05ef4284a6c54d33d3a3af3668e263
Soberek <59960384+Soberek@users.noreply.github.com>
2023-02-22 16:10:06 +0100

    style: bg image only visible above 600px width query

--- src/feature/Todo/index.tsx
+++ src/feature/Todo/index.tsx
@@ -1,18 +1,24 @@
 import { Box, useTheme } from '@mui/material';
 import { alpha } from '@mui/material';
-
+import useMediaQuery from '@mui/material/useMediaQuery/useMediaQuery';
 import TodoHeader from './TodoHeader';
 import TodoList from './TodoItemList';
 
 const Main = () => {
 	const theme = useTheme();
+	const mobileQuery = useMediaQuery('(max-width:600px)');
 
+	console.log();
+
+	const imageUrl =
+		'https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80';
+	// ${theme.palette.primary},
 	return (
 		<Box>
 			<Box
 				minHeight='100vh'
 				sx={{
-					backgroundImage: `url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')`,
+					background: mobileQuery ? theme.palette.primary.dark : `url(${imageUrl})`,
 					backgroundSize: 'cover',
 					backgroundPosition: 'center',
 					backgroundAttachment: 'fixed',
