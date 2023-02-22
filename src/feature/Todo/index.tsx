e526c8a4467801e79eeeb8a73834840ccd25ac0f
Soberek <59960384+Soberek@users.noreply.github.com>
2023-02-22 12:23:52 +0100

    style: bg img fixed

--- src/feature/Todo/index.tsx
+++ src/feature/Todo/index.tsx
@@ -10,24 +10,26 @@ const Main = () => {
 	return (
 		<Box>
 			<Box
-				height='100vh'
+				minHeight='100vh'
 				sx={{
 					backgroundImage: `
-                    linear-gradient(to bottom, rgba(255,255,255, 0) 30%, ${theme.palette.primary.dark} 70%,  ${
+                    linear-gradient(to bottom, rgba(255,255,255, 0) 10%, ${theme.palette.primary.dark} 100%,  ${
 						theme.palette.primary.dark
 					} 100%),
                     // linear-gradient(to left, rgba(255,0,0,0) 0%, ${alpha(theme.palette.primary.dark, 0.7)} 100%), 
                     url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')
                     `,
 					backgroundSize: 'cover',
-					backgroundPosition: 'bottom',
+					backgroundPosition: 'center',
+					backgroundAttachment: 'fixed',
 				}}
 				display='flex'
 				justifyContent='center'
 				alignItems='top'
+				pb={10}
 			>
 				{/* TODO Container */}
-				<Box mt={10} bgcolor='transparent' width={`95%`} maxWidth={`600px`} height={`400px`}>
+				<Box mt={10} bgcolor='transparent' width={`95%`} maxWidth={`600px`}>
 					{/* TODO HEADER */}
 					<TodoHeader />
 
